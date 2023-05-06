b2b3eb7ee327a61e62920f753717d92a21c40b90
haider-06418 <ma06418@st.habib.edu.pk>
2023-05-07 02:45:40 +0500

    created main.py

---
+++ code/main.py
@@ -0,0 +1,7 @@
+''' Task # 2: 
+Choose one of the two algorithms, SARSA and Q-learning, run it for various values of α and compare the two in terms of:
+- computational efficiency, i.e., the time taken to converge;
+- maximizing reward: Run the learned policies for each α value for various starting states and calculate the average reward to see 
+which α value results in the most average reward
+
+Choosen Algorithm: '''
